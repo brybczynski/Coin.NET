6f6dd7ce47568bb3ceeb6e2aa78744d3429377d5
Blazej Rybczynski <blazej.rybczynski@hotmail.com>
2015-04-26 08:39:52 +0200

    Add tests for changing value feature.

---
+++ src/Coin.NET.Tests/ChangingValue/ChangingValue.feature
@@ -0,0 +1,9 @@
+﻿Feature: ChangingValue
+	As a programmer 
+	I want to change value of a Coin object, 
+	so that I am able to store different amoun of money in this same object
+
+Scenario: Changing Value
+Given I have created a Coin object
+When I assign a new value to the Value property
+Then it's value is changed to provided one
---
+++ src/Coin.NET.Tests/ChangingValue/ChangingValueSteps.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace Coin.NET.Tests.ChangingValue
+{
+    [Binding]
+    public class ChangingValueSteps
+    {
+        private const decimal NewValue = 100.0M;
+
+        private Coin _coin;
+
+        [Given]
+        public void GivenIHaveCreatedACoinObject()
+        {
+            _coin = new Coin();
+        }
+
+        [When]
+        public void WhenIAssignANewValueToTheValueProperty()
+        {
+            _coin.Value = NewValue;
+        }
+
+        [Then]
+        public void ThenItSValueIsChangedToProvidedOne()
+        {
+            _coin.Value.Should().Be(NewValue);
+        }
+    }
+}
